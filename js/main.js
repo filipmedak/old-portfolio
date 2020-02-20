f3854a52e98d80ffa2faf7cb0076461d92d30be5
Filip Medak <filip.medak213@gmail.com>
2020-02-20 12:01:00 +0100

    Desktop edition

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
 $(function(){
+     //Responsive navigation toggle
     $("#toggle").click(function(){
         $("nav").toggleClass("fullNavHeight");
         $(".toggle").toggleClass("d-grid");
     });
+
+    // //Sticky navigation
+    // let previousScrollPosition = window.pageYOffset;
+
+    // window.onscroll = function(){
+    //     let currentScrollPosition = window.pageYOffset;
+    //     if (previousScrollPosition > currentScrollPosition){
+    //             $("nav").css({top: '0'});
+    //     }
+    //     else{
+    //         $("nav").css({top: '48px'});
+    //     }
+    //     previousScrollPosition = currentScrollPosition;
+    // }
+
+    //Remove classes
+    if ($(window).width() < 768) {
+        $('.spec').addClass('toggle');
+    } else {
+        $('.spec').removeClass('toggle');
+        $('#toggle').hide();
+        $('.logo').hide();
+    }
 });
