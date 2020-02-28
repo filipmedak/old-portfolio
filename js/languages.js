7ff43b9bafea754bdf7268e57f88bad331774741
Filip Medak <filip.medak213@gmail.com>
2020-02-28 10:50:22 +0100

    New update
    New logo, fixed navigation, font-scaling, JS tweaks, AOS animations, setup live projects, CV language options

---
+++ js/languages.js
@@ -0,0 +1,103 @@
+let arrLang = {
+    "en": {
+        /* Navigation text */
+        "PROJECTS" : "Projects",
+        "ABOUT" : "About",
+        "CONTACT" : "Contact",
+        "LANG1" : "English",
+        "LANG2" : "Croatian",
+
+        /* Landing text */
+        "LANDING1" : "I'm Filip.",
+        "LANDING2" : "a web designer",
+
+        /* About text */
+        "ABOUT1" : "Hello.",
+        "ABOUT2" : "I'm a web designer / multimedia student based in Zagreb, Croatia.",
+
+        /* Projects text */
+        "PROJECTS_TEXT" : "Projects.",
+        "LIVE_BUTTON" : "Live",
+        "CODE_BUTTON" : "Code",
+        "PROJECT1_NAME" : "Real Estate",
+        "PROJECT1_DESCRIPTION" : "Real Esate project created using HTML, CSS, PHP, mySQL and JS.",
+        "PROJECT2_NAME" : "Shift",
+        "PROJECT2_DESCRIPTION" : "Castlevania themed 2D platformer created in Construct2 for PC.",
+        "PROJECT3_NAME" : "City Cafe",
+        "PROJECT3_DESCRIPTION" : "Promotional video created for social media for City Cafe.",
+
+        /* About text */
+        "ABOUT_H2" : "What I can do.",
+        // "ABOUT_P1" : "",
+        // "ABOUT_P2" : "",
+        "ABOUT_CV" : "MY CV",
+        "CV_LANG1" : "ENG",
+        "CV_LANG2" : "CRO",
+
+        /* Contact text */
+        "CONTACT_H2" : "Let's Work Together.",
+        "CONTACT_A" : "Get in touch.",
+
+        /* FOOTER */
+        "FOOTER_P" : "© 2020 Filip Medak. All rights reserved."
+    },
+    "cro": {
+        /* Navigation text */
+        "PROJECTS" : "Projekti",
+        "ABOUT" : "O meni",
+        "CONTACT" : "Kontakt",
+        "LANG1" : "Engleski",
+        "LANG2" : "Hrvatski",
+
+        /* Landing text */
+        "LANDING1" : "Ja sam Filip.",
+        "LANDING2" : "web dizajner",
+        
+        /* About text */
+        "ABOUT1" : "Pozdrav.",
+        "ABOUT2" : "Ja sam web dizajner / student multimedijskog računarstva u Zagrebu, Hrvatskoj.",
+        
+        /* Projects text */
+        "PROJECTS_TEXT" : "Projekti.",
+        "LIVE_BUTTON" : "Uživo",
+        "CODE_BUTTON" : "Kod",
+        "PROJECT1_NAME" : "Real Estate",
+        "PROJECT1_DESCRIPTION" : "Oglasnik za nekretnine stvoren koristeći HTML, CSS, PHP, mySQL i JS.",
+        "PROJECT2_NAME" : "Shift",
+        "PROJECT2_DESCRIPTION" : "2D platformer na temu Castlevanie kreiran u Constructu 2 za PC.",
+        "PROJECT3_NAME" : "City Cafe",
+        "PROJECT3_DESCRIPTION" : "Promotivni video za društvene mreže za City Cafe.",
+        
+        /* About text */
+        "ABOUT_H2" : "Što mogu?",
+        "ABOUT_P1" : "Kreirati privlače web stranice koristeći HTML, CSS & JavaScript.",
+        "ABOUT_P2" : "Također sam upoznat sa mySQL-om, Jquery-em, PHP-om, SCSS-om & Bootstrap-om.",
+        "ABOUT_CV" : "ŽIVOTOPIS",
+        "CV_LANG1" : "ENG",
+        "CV_LANG2" : "HRV",
+        
+        /* Contact text */
+        "CONTACT_H2" : "Idemo raditi zajedno.",
+        "CONTACT_A" : "Kontaktiraj me.",
+        
+        /* FOOTER */
+        "FOOTER_P" : "© 2020 Filip Medak. Sva prava pridržana."
+    }
+  };
+  
+  $(document).ready(function() {
+    // The default language is English
+    let lang = "en";
+    $(".lang").each(function(index, element) {
+      $(this).text(arrLang[lang][$(this).attr("key")]);
+    });
+  });
+  
+  // get/set the selected language
+  $(".translate").click(function() {
+    let lang = $(this).attr("id");
+  
+    $(".lang").each(function(index, element) {
+      $(this).text(arrLang[lang][$(this).attr("key")]);
+    });
+  });
--- js/main.js
+++ js/main.js
@@ -5,16 +5,16 @@ $(function(){
         $(".toggle").toggleClass("d-grid");
     });
 
-    // //Sticky navigation
+    // //Hide navigation on scroll down
     // let previousScrollPosition = window.pageYOffset;
 
     // window.onscroll = function(){
     //     let currentScrollPosition = window.pageYOffset;
     //     if (previousScrollPosition > currentScrollPosition){
-    //             $("nav").css({top: '0'});
+    //         $('nav').css({'zIndex': 11000});
     //     }
     //     else{
-    //         $("nav").css({top: '48px'});
+    //         $('nav').css({'zIndex': -1});
     //     }
     //     previousScrollPosition = currentScrollPosition;
     // }
@@ -27,4 +27,38 @@ $(function(){
         $('#toggle').hide();
         $('.logo').hide();
     }
+
+    //Show languages
+    $('.dropdown').click(function(){
+        if($('.dropdownContent').css('display') == 'none'){
+            $('.dropdownContent').css('display', 'grid'); 
+        }
+
+        else{
+            $('.dropdownContent').css({'display':'none'});
+            $('.dropdown').css({'marginTop':'0px'});
+        }
+    });
+
+    //Hover hide un-active projects
+    $('.project').mouseenter(function(){
+        $(this).siblings().css({'opacity':'0.25'});
+    })
+    .mouseleave(function(){
+        $(this).siblings().css({'opacity':'1'});
+    });
+
+    //CV chose language
+    $('#CV').click(function(){
+        $(this).css({display: 'none'});
+        $('#aboutDetail').css({marginBottom: '120px'});
+
+        $('.CV_lang').animate({
+            opacity: 1
+        }, 100);
+    });
 });
+
+
+
+
